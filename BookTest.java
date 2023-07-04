c6b8833d04a94c3e9a6b2df3ed89f02f1d6f05f3
kyoungseok-Oh <134459438+kyoungseok-Oh@users.noreply.github.com>
2023-07-04 15:10:38 +0900

    Add files via upload

---
+++ BookTest.java
@@ -0,0 +1,17 @@
+public class BookTest {
+    public static void main(String[] args) {
+        //Q. [정수] 한 개를 저장할 변수를 선언하시오.
+        int a;
+        //Q. 한 권의 책 데이터를 저장하고 출력하시오. 6개의 변수로
+        String title="자바";
+        int price = 30000;
+        String company="패스트캠퍼스";
+        String author="박매일";
+        int page=700;
+        String isbn = "ISBN-111330011";
+        System.out.println(title+"\t"+price+"\t"+company+"\t"+author+"\t"+page+"\t"+isbn);
+        //Q. [책] 한 권을 저장할 변수를 선언하시오.
+        Book b = new Book();
+
+    }
+}
